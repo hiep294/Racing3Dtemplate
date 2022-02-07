7dc7b8cda09e2acf3c968e1dd3421eb10ea84fa2
hiep294 <quanghiep294@gmail.com>
2022-02-08 03:09:14 +0700

    + basic car

---
+++ Assets/Scripts/_note.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _note : MonoBehaviour
+{
+    /*
+     * 
+    xe: rigidbody > dynamic 
+    Thử chạy xe trên terrain => chạy được => terrain có collider và là static rigidbody
+
+
+    1 số đầu ra là nâng cao lúc này:
+    + chay xe trên 1 mặt dốc, hay bất kì không phải là mặt phẳng
+     */
+}
