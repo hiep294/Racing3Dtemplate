783b5cf6189e7327b11e97da226b53b233aa19f9
hiep294 <quanghiep294@gmail.com>
2022-02-08 22:30:47 +0700

    + check points

--- Assets/Scripts/LapCollider.cs
+++ Assets/Scripts/LapCollider.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lap : MonoBehaviour
+public class LapCollider : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SaveScript.LapNumber++;
-            SaveScript.LapChange = true;
+            other.GetComponent<SaveScript>().OnPlayerTriggerLapCollider();
         }
     }
 }
--- Assets/Scripts/SaveScript.cs
+++ Assets/Scripts/SaveScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,22 @@ using UnityStandardAssets.Vehicles.Car;
 public class SaveScript : MonoBehaviour
 {
     CarController playerCarController; //has boxCollider is trigger, but just to setup player to over to lap
-    public static int LapNumber = 0; // when player go to start point, => LapNumber changes
-    public static bool LapChange = false;
-    public static float LapTime;
-    public static float RaceTime;
-    public static float LastLapTime;
-    public static float BestLapTime;
+    int lapNumber = 0; // when player go to start point, => LapNumber changes
+    bool lapChange = false;
+    float lapTime;
+    float raceTime;
+    float lastLapTime;
+    float bestLapTime;
+
+    [SerializeField] CheckpointCollider[] checkpoints;
+
+    public int LapNumber { get => lapNumber; set => lapNumber = value; }
+    public bool LapChange { get => lapChange; set => lapChange = value; }
+    public float LapTime { get => lapTime; set => lapTime = value; }
+    public float RaceTime { get => raceTime; set => raceTime = value; }
+    public float LastLapTime { get => lastLapTime; set => lastLapTime = value; }
+    public float BestLapTime { get => bestLapTime; set => bestLapTime = value; }
+    public CheckpointCollider[] Checkpoints { get => checkpoints; }
 
     void Awake()
     {
@@ -20,24 +31,32 @@ public class SaveScript : MonoBehaviour
 
     private void Update()
     {
-        if (LapChange)
-        {
-            LapChange = false;
-            LastLapTime = LapTime;
-            // reset LapTime
-            LapTime = 0;
-
-            // calculate BestLap
-            if (LapNumber == 2 || (LapNumber > 2 && LastLapTime < BestLapTime))
-            {
-                BestLapTime = LastLapTime;
-            }
-        }
-
         if (LapNumber >= 1)
         {
             LapTime += Time.deltaTime;
             RaceTime += Time.deltaTime;
         }
     }
+
+    public void OnPlayerTriggerLapCollider()
+    {
+        LapNumber++;
+        LastLapTime = LapTime;
+        LapTime = 0;
+        foreach (var checkpoint in Checkpoints)
+        {
+            checkpoint.CheckPointPass = false;
+            checkpoint.LastCheckPointTime = checkpoint.ThisCheckPointTime;
+        }
+        CalculateBestLap();
+    }
+
+    void CalculateBestLap()
+    {
+        if (LapNumber == 2 || (LapNumber > 2 && LastLapTime < BestLapTime))
+        {
+            BestLapTime = LastLapTime;
+        }
+    }
+
 }
