faf1458dea19a7aa95415279f7e2dcd4a4fc67c9
Hiep <quanghiep294@gmail.com>
2022-02-09 15:33:57 +0700

    + processway points

---
+++ Assets/Scripts/JustToNote.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustToNote : MonoBehaviour
+{
+    [TextArea(15, 20)]
+    public string Description = "";
+}
